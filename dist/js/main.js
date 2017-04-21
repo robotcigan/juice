817047df4ee6892c311b8163889737b05444ef59
robotcigan <robotcigan@gmail.com>
2017-04-21 18:41:54 +0300

    21.04

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,53 @@
 $(document).ready(function () {
-  $('.news').each(function() {
-    var img = $(this).find('img');
-    var src = img.attr('src');
-    img.hide();
-    $(this).find('.news__img').css('background-image', 'url(' + src + ')');
+
+  $('.document-slider').slick({
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    arrows: true
   });
+
+  $('.juice-slider').slick({
+    dots: false,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+  $('.juice-slider-left').on('click', function() {
+    $('.juice-slider').slick("slickPrev");
+  })
+
+  $('.juice-slider-right').on('click', function() {
+    $('.juice-slider').slick("slickNext");
+  })
+
+  $('.taste-choice .taste').on('click', function() {
+    var index = $(this).index();
+    console.log(index)
+    $('.exposition__slide').removeClass('active');
+    $('.exposition__slide').eq(index).addClass('active');
+  })
+
 })
